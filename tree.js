1b02115f840c82993bf1b6932e145edb3901e99e
Binyameen <bmghafoor@gmail.com>
2023-07-31 19:44:48 -0400

    tree sum

--- tree.js
+++ tree.js
@@ -15,21 +15,30 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    if (!this.root) {
+      return 0;
+    }
+    let total = this.root.val;
+    function sumTree(node) {
+      for (let child of node.children) {
+        total += child.val;
+        if (child.children.length > 0) {
+          sumTree(child);
+        }
+      }
+    }
+    sumTree(this.root);
+    return total;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
-  countEvens() {
-
-  }
+  countEvens() {}
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
-  numGreater(lowerBound) {
-
-  }
+  numGreater(lowerBound) {}
 }
 
 module.exports = { Tree, TreeNode };
