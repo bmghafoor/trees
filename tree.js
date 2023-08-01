7d22b70075844bce5d20028cb87e7183f949fd2f
Binyameen <bmghafoor@gmail.com>
2023-07-31 20:07:21 -0400

    greaterThan

--- tree.js
+++ tree.js
@@ -33,12 +33,48 @@ class Tree {
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
-  countEvens() {}
+  countEvens() {
+    if (!this.root) {
+      return 0;
+    }
+    let count;
+    this.root.val % 2 === 0 ? (count = 1) : (count = 0);
+    function evens(node) {
+      for (let child of node.children) {
+        if (child.val % 2 === 0) {
+          count += 1;
+        }
+        if (child.children.length > 0) {
+          evens(child);
+        }
+      }
+    }
+    evens(this.root);
+    return count;
+  }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
-  numGreater(lowerBound) {}
+  numGreater(lowerBound) {
+    if (!this.root) {
+      return 0;
+    }
+    let count;
+    this.root.val > lowerBound ? (count = 1) : (count = 0);
+    function greaterThan(node) {
+      for (let child of node.children) {
+        if (child.val > lowerBound) {
+          count += 1;
+        }
+        if (child.children.length > 0) {
+          greaterThan(child);
+        }
+      }
+    }
+    greaterThan(this.root);
+    return count;
+  }
 }
 
 module.exports = { Tree, TreeNode };
